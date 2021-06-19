24d01231d5f228a01ead611bdac54f9a5f8a2d05
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-06-19 13:21:13 +0530

    add the home and main page dummy

---
+++ frontend/disschord/src/pages/mainPage/main-page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div className="page-container">
+                <div className="page-inner">
+                    This is the main page
+                </div>
+            </div>
+        )
+    }
+}
---
+++ frontend/disschord/src/pages/startupPage/startup-page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StartupPage extends React.Component {
+    render() {
+        return (
+            <div className="page-container">
+                <div className="page-inner">
+                    This is the startup page
+                </div>
+            </div>
+        )
+    }
+}
+
+export default StartupPage;
