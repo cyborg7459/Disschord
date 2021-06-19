453921518497e292e6c9c4c73e6c8b78c49daed6
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-06-19 14:23:04 +0530

    add the login logic

--- frontend/disschord/src/pages/loginPage/login-page.jsx
+++ frontend/disschord/src/pages/loginPage/login-page.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import axios from 'axios';
 
 class LoginPage extends React.Component {
+
+    login = async () => {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const credentials = {
+            email, 
+            password
+        }
+
+        const res = await axios.post('http://127.0.0.1:8080/api/v1/users/login', credentials);
+        console.log(res);
+    }
+
     render() {
         return (
             <div className="page-container">
@@ -9,6 +22,8 @@ class LoginPage extends React.Component {
                     <h1>Wecome to login page</h1>
                     <input type="text" id="email" placeholder="Enter your email adddress" />
                     <input type="password" id="password" placeholder="Enter your password" />
+                    <br />
+                    <button onClick={this.login}>Login</button>
                 </div>
             </div>
         )
