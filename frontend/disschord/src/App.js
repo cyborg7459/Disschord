d9a703d0500bef02a86fafa86eef676eb180b07e
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-06-19 13:22:40 +0530

    initial setup for files

--- frontend/disschord/src/App.js
+++ frontend/disschord/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import {Route, Switch} from 'react-router-dom';
+import StartupPage from './pages/startupPage/startup-page';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    displayMsg : ""
+  }
+
+  async componentDidMount() {
+    const data = await axios.get('http://localhost:8080/api/v1/test');
+    this.setState({
+      displayMsg : data.data.message
+    })
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route path="/" component={StartupPage}/>
+      </Switch>
+    )
+  }
 }
 
 export default App;
