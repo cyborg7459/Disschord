e26744c0ed5e54fc837fff76b51705982b12a8a3
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-07-09 01:39:21 +0530

    complete CRUD routes for posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,6 +1,28 @@
 const Post = require('../models/postModel');
 const appError = require('../utils/appError');
 
+exports.checkPostExistence = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.postID);
+        if(!post) return next(new appError('No post found with the given ID', 404));
+        req.post = post;
+        next();
+    }
+    catch(err) {
+        return next(err);
+    }
+}
+
+exports.checkPostOwnership = async (req, res, next) => {
+    try {
+        if(!req.post.byUser._id.equals(req.user._id)) return next(new appError('You are not authorized for this action', 403));
+        next();
+    }
+    catch(err) {
+        return next(err);
+    }
+}
+
 exports.getAllPosts = async (req, res, next) => {
     try {
         const posts = await Post.find({});
@@ -10,6 +32,18 @@ exports.getAllPosts = async (req, res, next) => {
             data: {
                 posts
             }
+        })
+    }
+    catch(err) {
+        return next(err);
+    }
+}
+
+exports.getSinglePost = async (req, res, next) => {
+    try {
+        res.status(200).json({
+            status: 'success',
+            data: req.post
         })
     }
     catch(err) {
@@ -36,5 +70,41 @@ exports.addNewPost = async (req, res, next) => {
     }
     catch(err) {
         next(err);
+    }
+}
+
+exports.updatePost = async (req, res, next) => {
+    try {
+        const updatedPost = {
+            title: req.body.title,
+            imageUrl: req.body.imageUrl,
+            content: req.body.content
+        }
+
+        const post = await Post.findByIdAndUpdate(req.post._id, updatedPost, {
+            runValidators: true
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: post
+        })
+    }
+    catch(err) {
+        next(err);
+    }
+}
+
+exports.deletePost = async (req, res, next) => {
+    try {
+        await Post.findByIdAndDelete(req.post._id);
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Post deleted successfully'
+        })
+    }
+    catch(err) {
+        return next(err);
     }
 }
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -7,4 +7,11 @@ router.route('/')
 .get(postController.getAllPosts)
 .post(postController.addNewPost)
 
+router.use('/:postID', postController.checkPostExistence);
+
+router.route('/:postID')
+.get(postController.getSinglePost)
+.patch(postController.checkPostOwnership, postController.updatePost)
+.delete(postController.checkPostOwnership, postController.deletePost)
+
 module.exports = router;
